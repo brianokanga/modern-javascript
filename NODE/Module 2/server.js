d180913fb3afc03461b19ee2450fc628baea8fb7
brianokanga <briannokanga@gmail.com>
2020-09-04 01:47:17 +0300

    added basic APi reading data from a json file

--- NODE/Module 2/server.js
+++ NODE/Module 2/server.js
@@ -1,16 +1,26 @@
+const fs = require ('fs');
 const http = require ('http');
 const url = require ('url');
 
 //SERVER
+
+const data = fs.readFile (`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse (data);
+
 const server = http.createServer ((req, res) => {
   const pathName = req.url;
   if (pathName === '/' || pathName === '/overview') {
     res.end ('This is the OVERVIEW');
   } else if (pathName === '/product') {
     res.end ('This is the RODUCT');
+  } else if (pathName === '/api') {
+    res.writeHead (200, {'Content-type': 'application/json'});
+    res.end (data);
   } else {
-    res.writeHead (404);
-    res.end ('This is the RODUCT');
+    res.writeHead (404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello-world',
+    });
   }
 });
 
